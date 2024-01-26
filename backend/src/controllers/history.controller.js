1d069f307d5b56ab53358be2e9dbe1fe6df56330
imontecalvo <imontecalvo@fi.uba.ar>
2024-01-26 12:45:25 -0300

    test /history + fix

--- backend/src/controllers/history.controller.js
+++ backend/src/controllers/history.controller.js
@@ -2,11 +2,35 @@
 // Se devuelve un array con las transacciones del usuario en la pagina indicada
 export const getHistory = async (req, res) => {
   const { user_id } = req.user;
-  const { page, items } = req.query;
+  let { page, items } = req.query;
+
+  if (!page || !items) {
+    return res.status(400).json({ msg: "Missing fields", ok: false });
+  }
+
+  try {
+    page = parseInt(page);
+    items = parseInt(items);
+  } catch (e) {
+    return res.status(400).json({ msg: "Invalid fields", ok: false });
+  }
+
+  if (
+    !Number.isInteger(page) ||
+    !Number.isInteger(items) ||
+    page <= 0 ||
+    items <= 0
+  ) {
+    return res.status(400).json({ msg: "Invalid fields", ok: false });
+  }
 
   const db = req.app.db;
 
   try {
+    if (!(await db.user.existUserById(user_id))) {
+      return res.status(400).json({ msg: "User not found", ok: false });
+    }
+
     const transactions = await db.transaction.getHistoryPaginated(
       user_id,
       items,
@@ -14,10 +38,10 @@ export const getHistory = async (req, res) => {
       [["date", "DESC"]]
     );
 
-    return res.status(201).json({ msg: transactions, ok: true });
+    return res.status(200).json({ msg: transactions, ok: true });
   } catch (error) {
     console.log(error);
-    return res.status(500).json({ msg: "Server error", ok: false });
+    return res.status(500).json({ error: "Server error", ok: false });
   }
 };
 
@@ -29,7 +53,7 @@ export const nOfTransactions = async (req, res) => {
 
   try {
     const nOfTransactions = await db.transaction.getNOfTransactions(user_id);
-    return res.status(201).json({ msg: nOfTransactions, ok: true });
+    return res.status(200).json({ msg: nOfTransactions, ok: true });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ msg: "Server error", ok: false });
